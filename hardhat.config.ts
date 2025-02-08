1f5fbb0ad4be18fae21cce6ca5c76a47c69cc55d
Luo <luoeth77@gmail.com>
2025-02-08 16:37:24 +0800

    增加hardhat.config.ts BASE_SEPOLIA 設定 增加.env 設定 修改ethers.parseUnits寫法

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,22 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config();
 
+const BASE_SEPOLIA_RPC_URL="https://sepolia.base.org";
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
-};
+  networks: {
+    // hardhat: {},
+    BASE_SEPOLIA: {
+      url: BASE_SEPOLIA_RPC_URL,
+      chainId: 84532,
+      accounts: [process.env.PRIVATE_KEY!],
+    }
+  },
+  mocha: {
+    timeout: 40000,
+  },
+}
 
 export default config;
--- test/deploy-dual-token.ts
+++ test/deploy-dual-token.ts
@@ -1,6 +1,4 @@
 import { ethers } from "hardhat";
-import { utils } from "ethers";
-
 
 async function main() {
     const [deployer] = await ethers.getSigners();
@@ -10,13 +8,13 @@ async function main() {
     // 取得 DualToken 合約工廠
     const DualToken = await ethers.getContractFactory("DualToken");
     
-    const initialSupplyToken1 = utils.parseUnits("5000000", 18);
-    const initialSupplyToken2 = utils.parseUnits("5000000", 18);
+    const initialSupplyToken1 = ethers.parseUnits("5000000", 18);
+    const initialSupplyToken2 = ethers.parseUnits("5000000", 18);
     
-    // 部署 DualToken 合约
+    // 部署 DualToken 合約
     const dualToken = await DualToken.deploy(initialSupplyToken1, initialSupplyToken2);
 
-    console.log("DualToken contract deployed to:", dualToken.address);
+    console.log("DualToken contract deployed to:", dualToken.target);
     console.log("Token1 address:", await dualToken.token1());
     console.log("Token2 address:", await dualToken.token2());
 }
